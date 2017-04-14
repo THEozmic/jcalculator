6550d3ffe87aa2c89b1a9bcfd1fa982839e9e28c
THEozmic <michaelozoemena2@gmail.com>
2017-04-14 16:16:40 +0100

    fixed multiple brackets bug

--- js/script.js
+++ js/script.js
@@ -1,17 +1,47 @@
 function solve (equation) {
   try {
-   patch = equation.split("(")[0]; // get the position of the first bracket
+    equation = equation.replace("x", "*"); // remove X and replace with *
+    patch = equation.split("(")[0]; // get the position of the first bracket
 
-   if (eval("'"+equation+"'") == patch) { // we do not have any thing to solve anymore
-    return eval(patch); // return the solution
-   }
-   /// calculate whatever is the the first bracket, add it to the part of the expression before the first bracket
-   patch += "*" + eval(equation.split("(")[1].split(")")[0]); 
-   patch = eval(patch); // get the answer of the first bracket and the part before the first bracket
-   other = equation.substring( equation.indexOf(')') + 1 ); // get the part after the first bracket
-   patch += other; // join the part after the first bracket to the part after it
-   return solve (patch); // redo the process, assuming we still have more stuff in other brackets
-  } catch (e) {
+    if (eval("'"+equation+"'") == patch) { // we do not have any thing to solve anymore
+      return eval(patch); // return the solution
+    }
+    // calculate whatever is the the first bracket, add it to the part of the expression before the first bracket
+    patch += "*" + eval(equation.split("(")[1].split(")")[0]); 
+    other = equation.substring( equation.indexOf(')') + 1 ); // get the part after the first bracket
+    patch += other; // join the part after the first bracket to the part after it
+    return solve (patch); // redo the process, assuming we still have more stuff in other brackets
+
+  }catch (e) {
     console.log ("An error has occured: [" + e + " ]"); // handle errors
   }
 }
+
+var prev = "";
+$(".btn").on("click", function () {
+
+  var action = $(this).attr("data-value").toLowerCase();
+
+  if(action !== "ce")
+    prev = $("#output").val();
+
+  switch (action) {
+
+    case "=":
+      $("#output").val(solve($("#output").val()));
+    break;
+
+    case "ce":
+      $("#output").val(prev.toString());
+    break;
+
+    case "c":
+       $("#output").val("");
+    break;
+
+    default:
+      $("#output").val($("#output").val() + action.toString());
+
+  }
+
+});
