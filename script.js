56efe33bcdaf1a5892b66641d421cd31b45c0fce
THEozmic <michaelozoemena2@gmail.com>
2017-04-14 13:05:35 +0100

    added comments

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function solve (equation) {
-   patch = equation.split("(")[0];
+  try {
+   patch = equation.split("(")[0]; // get the position of the first bracket
 
-   if (eval("'"+equation+"'") == patch) {
-    return eval(patch);
+   if (eval("'"+equation+"'") == patch) { // we do not have any thing to solve anymore
+    return eval(patch); // return the solution
    }
-   patch += "*" + eval(equation.split("(")[1].split(")")[0]);
-   patch = eval(patch);
-   other = equation.substring( equation.indexOf(')') + 1 );
-   patch += other; 
-   return solve (patch);
+   /// calculate whatever is the the first bracket, add it to the part of the expression before the first bracket
+   patch += "*" + eval(equation.split("(")[1].split(")")[0]); 
+   patch = eval(patch); // get the answer of the first bracket and the part before the first bracket
+   other = equation.substring( equation.indexOf(')') + 1 ); // get the part after the first bracket
+   patch += other; // join the part after the first bracket to the part after it
+   return solve (patch); // redo the process, assuming we still have more stuff in other brackets
+  } catch (e) {
+    console.log ("An error has occured: [" + e + " ]"); // handle errors
+  }
 }
-console.log(solve("5+5(4-9)"));
